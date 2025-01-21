e351b98d3234a86b962e18717fed518a297d5b93
VictorNVK <victorasd498@gmail.com>
2025-01-21 17:01:40 +0500

    init commit

---
+++ src/test/java/app/ImageTask/ImageTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ImageTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
